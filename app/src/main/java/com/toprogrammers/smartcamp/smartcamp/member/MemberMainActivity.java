119f391f4ad3a6e76808cfc28746a79c1ef8bddf
emilg1101 <emilyandro99@gmail.com>
2016-11-12 16:57:16 +0300

    Pocti work

--- app/src/main/java/com/toprogrammers/smartcamp/smartcamp/member/MemberMainActivity.java
+++ app/src/main/java/com/toprogrammers/smartcamp/smartcamp/member/MemberMainActivity.java
@@ -36,215 +36,64 @@ import com.toprogrammers.smartcamp.smartcamp.objects.User;
 
 public class MemberMainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    public static SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
-    private TabLayout tabLayout;
-    static ProgressDialog progressDialog;
-    static InternetConnection ic;
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 2 ;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_member_main);
 
         SharedPreferences sharedPreferences = getSharedPreferences("APP", Context.MODE_PRIVATE);
         if(!sharedPreferences.getBoolean("userIsLoggedIn", false)){
             startActivity(new Intent(MemberMainActivity.this, LoginActivity.class));
             finish();
         }
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        setContentView(R.layout.activity_member_main);
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setOffscreenPageLimit(0);
+        viewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        tabLayout.post(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
             }
         });
     }
 
+    class MyAdapter extends FragmentPagerAdapter {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_member_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-        Schedule schedule;
-        User user;
-        View rootView;
-        int dataPosition;
-        String response;
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            switch (getArguments().getInt(ARG_SECTION_NUMBER)){
-                case 2:rootView = inflater.inflate(R.layout.fragment_member_tasks, container, false);
-                    break;
-                default:rootView = inflater.inflate(R.layout.fragment_member_timetable, container, false);
-                    break;
-            }
-            return rootView;
-        }
-
-        @Override
-        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-            super.onViewCreated(view, savedInstanceState);
-
-            user = new User(getActivity());
-            dataPosition = 0;
-            if(getArguments().getInt(ARG_SECTION_NUMBER)==1){
-                new ScheduleTask().execute();
-            }
-        }
-
-        public class ScheduleTask extends AsyncTask<Void, Void, Boolean> {
-
-            @Override
-            protected void onPreExecute() {
-                progressDialog = new ProgressDialog(getActivity());
-                progressDialog.setIndeterminate(true);
-                progressDialog.setMessage("Загрузка...");
-                progressDialog.setCancelable(true);
-            }
-
-            @Override
-            protected Boolean doInBackground(Void... params) {
-
-                response = ic.makeGETrequest(
-                        new String[] {
-                                "method", "schedule.get",
-                                "tour_id", String.valueOf(user.getTourId())
-                        });
-
-                return true;
-            }
-
-            @Override
-            protected void onPostExecute(final Boolean success) {
-                progressDialog.dismiss();
-                schedule = new Schedule(getContext(), response);
-                schedule.updateInfo();
-                TimetableListAdapter listAdapter = new TimetableListAdapter(getActivity(), schedule, dataPosition);
-                ListView listView = (ListView) rootView.findViewById(R.id.timetable);
-                listView.setAdapter(listAdapter);
-            }
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
+        public MyAdapter(FragmentManager fm) {
             super(fm);
         }
 
         @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+        public android.support.v4.app.Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 : return new FragmentTimeTable();
+                case 1 : return new FragmentTasks();
+            }
+            return null;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 2;
+            return int_items;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "РАСПИСАНИЕ";
-                case 1:
-                    return "ЗАДАНИЯ";
+
+            switch (position){
+                case 0 :
+                    return "Расписание";
+                case 1 :
+                    return "Задачи";
             }
             return null;
         }
